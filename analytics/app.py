aad5696dc5b78a6927623749192221c60d99fe4c
“Elisha <elishaomokaro44@gmail.comgit config --global user.name Elisha Omokarogit config --global user.email elishaomokaro44@gmail.com>
2026-09-22 00:51:25 -0700

    Initial commit with .gitignore

---
+++ analytics/app.py
@@ -0,0 +1,91 @@
+import logging
+import os
+import json
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
+from flask import jsonify, request, Response
+from sqlalchemy import and_, text
+from random import randint
+
+from config import app, db
+
+
+port_number = int(os.environ.get("APP_PORT", 5153))
+
+
+@app.route("/health_check")
+def health_check():
+    return "ok"
+
+
+@app.route("/readiness_check")
+def readiness_check():
+    try:
+        count = db.session.query(Token).count()
+    except Exception as e:
+        app.logger.error(e)
+        return "failed", 500
+    else:
+        return "ok"
+
+
+def get_daily_visits():
+    with app.app_context():
+        try:
+            result = db.session.execute(text("""
+            SELECT Date(created_at) AS date,
+                   Count(*)         AS visits
+            FROM   tokens
+            WHERE  used_at IS NOT NULL
+            GROUP  BY Date(created_at)
+            """))
+
+            response = {}
+            for row in result:
+                response[str(row[0])] = int(row[1])  # force int
+
+            app.logger.info(f"get_daily_visits response: {response}")
+            return response
+        except Exception as e:
+            app.logger.error(f"Error in get_daily_visits: {e}")
+            return {"error": str(e)}
+
+
+@app.route("/api/reports/daily_usage")
+def daily_visits():
+    data = get_daily_visits()
+    # use Response/json.dumps to force serialization
+    return Response(json.dumps(data), mimetype="application/json")
+
+
+@app.route("/api/reports/user_visits", methods=["GET"])
+def all_user_visits():
+    result = db.session.execute(text("""
+    SELECT t.user_id,
+           t.visits,
+           users.joined_at
+    FROM   (SELECT tokens.user_id,
+                   Count(*) AS visits
+            FROM   tokens
+            GROUP  BY user_id) AS t
+           LEFT JOIN users
+                  ON t.user_id = users.id;
+    """))
+
+    response = {}
+    for row in result:
+        response[row[0]] = {
+            "visits": int(row[1]),
+            "joined_at": str(row[2])
+        }
+    
+    return jsonify(response)
+
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(get_daily_visits, 'interval', seconds=30)
+scheduler.start()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port_number)
---
+++ analytics/config.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db_username = os.environ["DB_USERNAME"]
+db_password = os.environ["DB_PASSWORD"]
+db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "postgres")
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mac@127.0.0.1:5432/analytics"
+db = SQLAlchemy(app)
+
+app.logger.setLevel(logging.DEBUG)
